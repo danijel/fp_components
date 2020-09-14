0647f4d5c15d77263a359c2657abc57469ff3be3
Danijel <danijel@awellhealth.com>
2020-09-14 13:28:52 +0200

    Readme and header update
    # |<----            Using a Maximum Of 72 Characters              ---->|
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23 or DEV-### for Jira
    
    # --- COMMIT END ---
    # GIT close issue
    # close, closes, closed, fixes, fixed followed by #<Issue number>
    #
    # Type can be
    #    feat     (new feature)
    #    fix      (bug fix)
    #    refactor (refactoring production code)
    #    style    (formatting, missing semi colons, etc; no code change)
    #    docs     (changes to documentation)
    #    test     (adding or refactoring tests; no production code change)
    #    chore    (updating grunt tasks etc; no production code change)
    # --------------------
    # Scope can be
    #    Client: scope should usually be the affected page
    #    Server: scope should usually be the affected method or publication
    # --------------------
    # Remember to
    #   - Capitalize the subject line
    #   - Use the imperative mood in the subject line
    #   - Do not end the subject line with a period
    #   - Separate subject from body with a blank line
    #   - Use the body to explain what and why vs. how
    #   - Can use multiple lines with "-" for bullet points in body

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from './logo.svg'
+import ramda from './ramda.png'
 import './App.css'
 import { DEMO_POSTS } from './fixtures/post_fixtures'
 import { Posts } from './components/Posts'
@@ -15,7 +16,16 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          React
+        </a>
+        <img src={ramda} className='Ramda-logo' alt='ramda_logo' />
+        <a
+          className='App-link'
+          href='https://ramdajs.com/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Ramda
         </a>
       </header>
       <Posts posts={DEMO_POSTS} />
