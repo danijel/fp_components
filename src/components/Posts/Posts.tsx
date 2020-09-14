19192e93dfed355aca5f5f8b61cdfb14f352afbf
Danijel <danijel@awellhealth.com>
2020-09-14 12:56:45 +0200

    refactored to use ramda FP for components
    # |<----            Using a Maximum Of 72 Characters              ---->|
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23 or DEV-### for Jira
    
    # --- COMMIT END ---
    # GIT close issue
    # close, closes, closed, fixes, fixed followed by #<Issue number>
    #
    # Type can be
    #    feat     (new feature)
    #    fix      (bug fix)
    #    refactor (refactoring production code)
    #    style    (formatting, missing semi colons, etc; no code change)
    #    docs     (changes to documentation)
    #    test     (adding or refactoring tests; no production code change)
    #    chore    (updating grunt tasks etc; no production code change)
    # --------------------
    # Scope can be
    #    Client: scope should usually be the affected page
    #    Server: scope should usually be the affected method or publication
    # --------------------
    # Remember to
    #   - Capitalize the subject line
    #   - Use the imperative mood in the subject line
    #   - Do not end the subject line with a period
    #   - Separate subject from body with a blank line
    #   - Use the body to explain what and why vs. how
    #   - Can use multiple lines with "-" for bullet points in body

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,40 +1,96 @@
 import React, { FC } from 'react'
-import { Post } from './types'
+import * as R from 'ramda'
+import { Post, Comment } from './types'
 import './styles.css'
 
 interface Props {
   posts: Post[]
 }
 
+const isMissing = R.compose(R.isEmpty, R.defaultTo([]))
+const Text = (text: string) => <p>{text}</p>
+const Author = (author: string) => <h5>{author}</h5>
+
+const commentHtml = R.compose<Comment, Comment, Comment>(
+  R.over(R.lensProp('text'), Text),
+  R.over(R.lensProp('author'), Author),
+)
+
+const Reply = ({ id, text, author }: Comment) => (
+  <div className='reply-row' key={id}>
+    {text}
+    {author}
+  </div>
+)
+
+const ReplyWrapper = R.compose<
+  Comment[] | undefined,
+  Comment[],
+  Comment[],
+  JSX.Element[]
+>(R.map(Reply), R.map<Comment, Comment>(commentHtml), R.defaultTo([]))
+
+const RepliesWrapper = (comment: Comment) => (
+  <div key={comment.id}>
+    <h4>Replies</h4>
+    {ReplyWrapper(comment.replies)}
+  </div>
+)
+
+const Replies = (comment: Comment) =>
+  R.ifElse(
+    () => isMissing(comment.replies),
+    () => null,
+    RepliesWrapper,
+  )(comment)
+
+const CommentRow = (comment: Comment) => {
+  const { id, text, author } = comment
+  return (
+    <div className='comment-row' key={id}>
+      {text}
+      {author}
+      {Replies(comment)}
+    </div>
+  )
+}
+
+const CommentWrapper = R.compose<
+  Comment[] | undefined,
+  Comment[],
+  Comment[],
+  JSX.Element[]
+>(R.map(CommentRow), R.map<Comment, Comment>(commentHtml), R.defaultTo([]))
+
+const CommentsWrapper = (post: Post) => (
+  <div key={post.id}>
+    <h3>Comments</h3>
+    {CommentWrapper(post.comments)}
+  </div>
+)
+
+const Comments = (post: Post) =>
+  R.ifElse(
+    () => isMissing(post.comments),
+    () => null,
+    CommentsWrapper,
+  )(post)
+
+const PostWrapper = ({ post }: { post: Post }) => {
+  const { id, title, text } = post
+  return (
+    <div className='row' key={id}>
+      <h2>{title}</h2>
+      {Text(text)}
+      {Comments(post)}
+    </div>
+  )
+}
+
 const Posts: FC<Props> = ({ posts }) => (
   <div>
-    {posts.map(({ id, title, text, comments }) => (
-      <div key={id}>
-        <div className='row'>
-          <h2>{title}</h2>
-          <p>{text}</p>
-        </div>
-        {comments && <h3>Comments</h3>}
-        {comments &&
-          comments.map(
-            ({ id: commentId, text: commentText, author, replies }) => (
-              <div className='comment-row' key={commentId}>
-                <p>{commentText}</p>
-                <h5>{author}</h5>
-                {replies && <h4>Replies</h4>}
-                {replies &&
-                  replies.map(
-                    ({ id: replyId, text: replyText, author: replyAuthor }) => (
-                      <div className='reply-row' key={replyId}>
-                        <p>{replyText}</p>
-                        <h5>{replyAuthor}</h5>
-                      </div>
-                    ),
-                  )}
-              </div>
-            ),
-          )}
-      </div>
+    {posts.map(post => (
+      <PostWrapper key={post.id} post={post} />
     ))}
   </div>
 )
